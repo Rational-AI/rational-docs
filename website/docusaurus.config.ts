32d85558ea8d2c2d5e82fd060031065799e22775
diesys <d.turtulici@geckosoft.it>
2026-01-29 19:45:59 +0100

    chore(doc): bootstrap algolia (commented)

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -73,6 +73,43 @@ const config: Config = {
   ],
 
   themeConfig: {
+    // algolia: {
+    //   // TODO: ALGOLIA SEARCH add KEY
+    //   // The application ID provided by Algolia
+    //   appId: "YOUR_APP_ID",
+
+    //   // TODO: ALGOLIA SEARCH add KEY
+    //   // Public API key: it is safe to commit it
+    //   apiKey: "YOUR_SEARCH_API_KEY",
+
+    //   // TODO: ALGOLIA SEARCH add NAME
+    //   indexName: "YOUR_INDEX_NAME",
+
+    //   // TODO: ALGOLIA SEARCH FIXME: check wanted configuration
+    //   // Optional: see doc section below
+    //   contextualSearch: true,
+
+    //   // // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+    //   // externalUrlRegex: "external\\.com|domain\\.com",
+
+    //   // // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+    //   // replaceSearchResultPathname: {
+    //   //   from: "/docs/", // or as RegExp: /\/docs\//
+    //   //   to: "/",
+    //   // },
+
+    //   // // Optional: Algolia search parameters
+    //   // searchParameters: {},
+
+    //   // // Optional: path for search page that enabled by default (`false` to disable it)
+    //   // searchPagePath: "search",
+
+    //   // // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+    //   // insights: false,
+
+    //   // // Optional: whether you want to use the new Ask AI feature (undefined by default)
+    //   // askAi: "YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID",
+    // },
     image: "img/rational-social-card.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
