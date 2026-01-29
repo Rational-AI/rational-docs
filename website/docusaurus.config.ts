46aabf2283613bb1e6841a1fa8b232dc9d0c76e4
diesys <d.turtulici@geckosoft.it>
2026-01-29 19:56:49 +0100

    chore(doc): biome run `check --write`

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -5,202 +5,202 @@ import { themes as prismThemes } from "prism-react-renderer";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "Rational AI",
-  tagline: "Your Data, Your AI",
-  favicon: "img/favicon.ico",
+	title: "Rational AI",
+	tagline: "Your Data, Your AI",
+	favicon: "img/favicon.ico",
 
-  // TODO: ALGOLIA SEARCH
-  // Theme for algolia search
-  // themes: ["@docusaurus/theme-search-algolia"],
+	// TODO: ALGOLIA SEARCH
+	// Theme for algolia search
+	// themes: ["@docusaurus/theme-search-algolia"],
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
+	// Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+	future: {
+		v4: true, // Improve compatibility with the upcoming Docusaurus v4
+	},
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+	// Set the production url of your site here
+	url: "https://your-docusaurus-site.example.com",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "rational", // Usually your GitHub org/user name.
-  projectName: "rational", // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "rational", // Usually your GitHub org/user name.
+	projectName: "rational", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+	onBrokenLinks: "throw",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "it"],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "it"],
+	},
 
-  presets: [
-    [
-      "classic",
-      {
-        docs: {
-          sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ["rss", "atom"],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
-        },
-        theme: {
-          customCss: "./src/css/custom.css",
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+	presets: [
+		[
+			"classic",
+			{
+				docs: {
+					sidebarPath: "./sidebars.ts",
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+				},
+				blog: {
+					showReadingTime: true,
+					feedOptions: {
+						type: ["rss", "atom"],
+						xslt: true,
+					},
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+					// Useful options to enforce blogging best practices
+					onInlineTags: "warn",
+					onInlineAuthors: "warn",
+					onUntruncatedBlogPosts: "warn",
+				},
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    // algolia: {
-    //   // TODO: ALGOLIA SEARCH add KEY
-    //   // The application ID provided by Algolia
-    //   appId: "YOUR_APP_ID",
+	themeConfig: {
+		// algolia: {
+		//   // TODO: ALGOLIA SEARCH add KEY
+		//   // The application ID provided by Algolia
+		//   appId: "YOUR_APP_ID",
 
-    //   // TODO: ALGOLIA SEARCH add KEY
-    //   // Public API key: it is safe to commit it
-    //   apiKey: "YOUR_SEARCH_API_KEY",
+		//   // TODO: ALGOLIA SEARCH add KEY
+		//   // Public API key: it is safe to commit it
+		//   apiKey: "YOUR_SEARCH_API_KEY",
 
-    //   // TODO: ALGOLIA SEARCH add NAME
-    //   indexName: "YOUR_INDEX_NAME",
+		//   // TODO: ALGOLIA SEARCH add NAME
+		//   indexName: "YOUR_INDEX_NAME",
 
-    //   // TODO: ALGOLIA SEARCH FIXME: check wanted configuration
-    //   // Optional: see doc section below
-    //   contextualSearch: true,
+		//   // TODO: ALGOLIA SEARCH FIXME: check wanted configuration
+		//   // Optional: see doc section below
+		//   contextualSearch: true,
 
-    //   // // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-    //   // externalUrlRegex: "external\\.com|domain\\.com",
+		//   // // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+		//   // externalUrlRegex: "external\\.com|domain\\.com",
 
-    //   // // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-    //   // replaceSearchResultPathname: {
-    //   //   from: "/docs/", // or as RegExp: /\/docs\//
-    //   //   to: "/",
-    //   // },
+		//   // // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+		//   // replaceSearchResultPathname: {
+		//   //   from: "/docs/", // or as RegExp: /\/docs\//
+		//   //   to: "/",
+		//   // },
 
-    //   // // Optional: Algolia search parameters
-    //   // searchParameters: {},
+		//   // // Optional: Algolia search parameters
+		//   // searchParameters: {},
 
-    //   // // Optional: path for search page that enabled by default (`false` to disable it)
-    //   // searchPagePath: "search",
+		//   // // Optional: path for search page that enabled by default (`false` to disable it)
+		//   // searchPagePath: "search",
 
-    //   // // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
-    //   // insights: false,
+		//   // // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+		//   // insights: false,
 
-    //   // // Optional: whether you want to use the new Ask AI feature (undefined by default)
-    //   // askAi: "YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID",
-    // },
-    image: "img/rational-social-card.jpg",
-    colorMode: {
-      respectPrefersColorScheme: true,
-    },
-    navbar: {
-      title: "Rational AI",
-      logo: {
-        alt: "Rational AI",
-        src: "img/logo.svg",
-      },
-      items: [
-        {
-          type: "docSidebar",
-          sidebarId: "docsSidebar",
-          position: "left",
-          label: "Docs",
-        },
-        {
-          type: "search",
-          position: "left",
-        },
-        // {
-        //   href: "https://github.com/Rational-AI/",
-        //   label: "GitHub",
-        //   position: "right",
-        // },
-        // { to: "/blog", label: "Blog", position: "left" },
-        {
-          type: "docsVersionDropdown",
-          position: "right",
-        },
-        {
-          type: "localeDropdown",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      links: [
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Docs",
-              to: "/docs/intro",
-            },
-          ],
-        },
-        {
-          title: "Resources",
-          items: [
-            {
-              label: "Rational AI",
-              to: "https://rational.is",
-            },
-            // {
-            //   label: "Stack Overflow",
-            //   href: "",
-            // },
-            // {
-            //   label: "Discord",
-            //   href: "",
-            // },
-            // {
-            //   label: "X",
-            //   href: "",
-            // },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "Blog",
-              to: "/blog",
-            },
-            {
-              label: "GitHub",
-              href: "https://github.com/Rational-AI/",
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Rational AI, Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+		//   // // Optional: whether you want to use the new Ask AI feature (undefined by default)
+		//   // askAi: "YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID",
+		// },
+		image: "img/rational-social-card.jpg",
+		colorMode: {
+			respectPrefersColorScheme: true,
+		},
+		navbar: {
+			title: "Rational AI",
+			logo: {
+				alt: "Rational AI",
+				src: "img/logo.svg",
+			},
+			items: [
+				{
+					type: "docSidebar",
+					sidebarId: "docsSidebar",
+					position: "left",
+					label: "Docs",
+				},
+				{
+					type: "search",
+					position: "left",
+				},
+				// {
+				//   href: "https://github.com/Rational-AI/",
+				//   label: "GitHub",
+				//   position: "right",
+				// },
+				// { to: "/blog", label: "Blog", position: "left" },
+				{
+					type: "docsVersionDropdown",
+					position: "right",
+				},
+				{
+					type: "localeDropdown",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "dark",
+			links: [
+				{
+					title: "Docs",
+					items: [
+						{
+							label: "Docs",
+							to: "/docs/intro",
+						},
+					],
+				},
+				{
+					title: "Resources",
+					items: [
+						{
+							label: "Rational AI",
+							to: "https://rational.is",
+						},
+						// {
+						//   label: "Stack Overflow",
+						//   href: "",
+						// },
+						// {
+						//   label: "Discord",
+						//   href: "",
+						// },
+						// {
+						//   label: "X",
+						//   href: "",
+						// },
+					],
+				},
+				{
+					title: "More",
+					items: [
+						{
+							label: "Blog",
+							to: "/blog",
+						},
+						{
+							label: "GitHub",
+							href: "https://github.com/Rational-AI/",
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} Rational AI, Built with Docusaurus.`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 };
 
 export default config;
