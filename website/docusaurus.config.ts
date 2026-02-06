4efcc26b2025a4c86763fbe16e0a038420fb3f99
Andrea Canciani <a.canciani@geckosoft.it>
2026-02-06 17:43:20 +0100

    chore: drop/hide blog

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -53,21 +53,22 @@ const config: Config = {
 					editUrl:
 						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
 				},
-				blog: {
-					showReadingTime: true,
-					feedOptions: {
-						type: ["rss", "atom"],
-						xslt: true,
-					},
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl:
-						"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-					// Useful options to enforce blogging best practices
-					onInlineTags: "warn",
-					onInlineAuthors: "warn",
-					onUntruncatedBlogPosts: "warn",
-				},
+				blog: false,
+				// blog: {
+				// 	showReadingTime: true,
+				// 	feedOptions: {
+				// 		type: ["rss", "atom"],
+				// 		xslt: true,
+				// 	},
+				// 	// Please change this to your repo.
+				// 	// Remove this to remove the "edit this page" links.
+				// 	editUrl:
+				// 		"https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+				// 	// Useful options to enforce blogging best practices
+				// 	onInlineTags: "warn",
+				// 	onInlineAuthors: "warn",
+				// 	onUntruncatedBlogPosts: "warn",
+				// },
 				theme: {
 					customCss: "./src/css/custom.css",
 				},
@@ -202,10 +203,10 @@ const config: Config = {
 				{
 					title: "More",
 					items: [
-						{
-							label: "Blog",
-							to: "/blog",
-						},
+						// {
+						// 	label: "Blog",
+						// 	to: "/blog",
+						// },
 						{
 							label: "GitHub",
 							href: "https://github.com/Rational-AI/",
