e04d825c4724be72425784e0091326f3f8af2712
Andrea Canciani <a.canciani@geckosoft.it>
2026-02-06 09:10:48 +0100

    fix: update configuration for GitHub Pages

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -19,15 +19,15 @@ const config: Config = {
 	},
 
 	// Set the production url of your site here
-	url: "https://your-docusaurus-site.example.com",
+	url: "https://rational-ai.github.io",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+	baseUrl: "/rational-docs/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "rational", // Usually your GitHub org/user name.
-	projectName: "rational", // Usually your repo name.
+	organizationName: "rational-ai", // Usually your GitHub org/user name.
+	projectName: "rational-docs", // Usually your repo name.
 
 	onBrokenLinks: "throw",
 
