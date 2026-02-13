6a49f084194a73e97420873d089d68b9a4472b9e
Andrea Canciani <a.canciani@geckosoft.it>
2026-02-13 17:13:34 +0100

    fix: update url configuration

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -19,10 +19,10 @@ const config: Config = {
 	},
 
 	// Set the production url of your site here
-	url: "https://rational-ai.github.io",
+	url: "https://docs.rational.is",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/rational-docs/",
+	baseUrl: "/",
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
